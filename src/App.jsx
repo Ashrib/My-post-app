04bc8cb82985fc9a74d7e20802ec627d75947791
Syed Kariz Ali <karizali310@gmail.com>
2022-11-10 21:17:03 +0500

    css update

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,26 @@ import './App.css';
 import {Route , Routes, Link} from 'react-router-dom';
 import SignUp from './components/signup/signup.jsx';
 import LogIn from './components/login/login.jsx';
+import { useState } from 'react';
 
 function App() {
   // const lik = window.location.href();
   // console.log(lik)
+  const [isLogin, setIsLogin] = useState(false);
   return (
-    <div >
-      <nav>
-      <ul>
-        <li><Link to={`./components/signup/signup`}>Signup</Link></li>
-        <li><Link to={`./components/login/login`}>login</Link></li>
-      </ul>
-      </nav>
+    <div id='body-div'>
+      {
+        (isLogin)?
+        <nav>
+        <ul>
+          <li><Link to={`./components/signup/signup`}>Signup</Link></li>
+          <li><Link to={`./components/login/login`}>login</Link></li>
+        </ul>
+        </nav>
+        :
+        null
+      }
+
       <div id='container'>
       <Routes>
         <Route path="/" element={<LogIn />}/>
